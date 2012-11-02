c017ae38ca34f50b67b1919501bf5530fa91582f
Jim John <rjjm2k@gmail.com>
2012-11-02 22:45:07 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lebase::Application.config.secret_token = '15e6a299656e357603600b042c1789794bc6b655a99eb3e6093203ae7b77020f4e827c0c04348cce0fd984b7d1be07ed6b8c19abda6a82fd94d34086a50df4c4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Lebase::Application.config.session_store :cookie_store, key: '_lebase_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Lebase::Application.config.session_store :active_record_store
